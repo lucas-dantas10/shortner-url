10b22f13f65cd8ec52d8f97acd4ad6a63160607f
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-12-07 22:22:11 -0300

    feat: create entity url of cassandra

---
+++ src/main/java/br/com/shortener_url/infra/persistence/Url.java
@@ -0,0 +1,26 @@
+package br.com.shortener_url.infra.persistence;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table("url")
+public class Url {
+
+    @PrimaryKey("shortcode")
+    private Long id;
+
+    @Column("long_url")
+    private String longUrl;
+
+    @Column("created_at")
+    private LocalDateTime createdAt;
+}
